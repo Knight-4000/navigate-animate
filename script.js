cd8e1b43b352511f5555a45d878f1fa9c8104edc
Knight-4000 <yorksprinters@gmail.com>
2023-05-07 09:54:00 -0400

    Trimmed code. Added navlink animation.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const menuBars = document.getElementById('menu-bars');
 const overlay = document.getElementById('overlay');
-const nav1 = document.getElementById('nav1');
-const nav2 = document.getElementById('nav2');
-const nav3 = document.getElementById('nav3');
-const nav4 = document.getElementById('nav4');
-const nav5 = document.getElementById('nav5');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control Nav Links Animation
+
+function navAnimation(waxon, waxoff) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${waxon}-${i + 1 }`, `slide-${waxoff}-${i + 1}`);
+    })
+}
 
 function toggleNav() {
     // Toggle for menu bars
@@ -13,13 +22,14 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
     // Animate In - Overlay
-        overlay.classList.remove('overlay-slide-left');
-        overlay.classList.add('overlay-slide-right');
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        navAnimation('out', 'in');
     } else {
-        overlay.classList.remove('overlay-slide-right');
-        overlay.classList.add('overlay-slide-left'); 
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        navAnimation('in', 'out');
     }
 }
+
 // Event Listeners 
 
 menuBars.addEventListener('click', toggleNav);
